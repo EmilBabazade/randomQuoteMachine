e288d7904281c8ebc66a1190da752471214f0339
emil <emilbabazade29@gmail.com>
2021-05-02 20:02:35 +0300

    styling positioning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
 import {getQuotes} from './services/Quote'
+import {createUseStyles} from 'react-jss'
+import './index.css' // box sizing and stuff
 
+const useStyles = createUseStyles({
+    container: {
+        width: '100%',
+        height: '100%',
+
+        '& *': {
+            margin: 0
+        }
+    },
+    quoteBox: {
+        // center
+        border: '2px solid red',
+        position: 'absolute',
+        top: '30%',
+        left: '25%',
+        // size 
+        width: '50%',
+        height: '40%',
+        // center inside
+        padding: '20px',
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+})
 
 
 const App = () => {
     const [currQuote, setCurrQuote] = useState(null)
     const [quotes, setQuotes] = useState([])
+
+    const styles = useStyles()
 
     // initialize quotes and currentQuote
     useEffect(() => {
@@ -27,10 +57,12 @@ const App = () => {
 
     if(currQuote) 
         return (
-            <div>
-                <h1>{currQuote.quote}</h1>
-                <p>{currQuote.author}</p>
-                <button onClick={newQuote}>New Quote</button>
+            <div className={styles.container}>
+                <div className={styles.quoteBox}>
+                    <h1>{currQuote.quote}</h1>
+                    <p>- {currQuote.author}</p>
+                    <button onClick={newQuote}>New Quote</button>
+                </div>
             </div>
         )
     
