4795a0049c46107dafc05d556e39d9a86c470310
emil <emilbabazade29@gmail.com>
2021-05-02 17:48:26 +0300

    get all quotes, and initialize current Quote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState, useEffect} from 'react'
+import {getQuotes} from './services/Quote'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [currQuote, setCurrQuote] = useState({
+        author: '1', quote: '1'
+    })
+    const [quotes, setQuotes] = useState([])
+
+    // initialize quotes and currentQuote
+    useEffect(() => {
+        getQuotes()
+            .then(quotes => {
+                setQuotes(quotes)
+                const index = Math.floor(Math.random() * (quotes.length - 1))
+                setCurrQuote(quotes[index])
+            })
+    }, [])
+
+    return (
+        <div>
+            {currQuote.quote}
+            {/* {currQuote.author} */}
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/services/Quote.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const QUOTES_SOURCE = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+const getQuotes = () => {
+    return axios.get(QUOTES_SOURCE).then(res => res.data.quotes)
+}
+
+export {
+    getQuotes
+}
